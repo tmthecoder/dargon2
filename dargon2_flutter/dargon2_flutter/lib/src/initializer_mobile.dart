768fc3b4cc7d90709294a5ef4e3770bdc9325bd6
Tejas Mehta <tmthecoder@gmail.com>
2022-01-24 01:15:02 -0500

    change ndk version & add desktop initializers

--- dargon2_flutter/dargon2_flutter/lib/src/initializer_mobile.dart
+++ dargon2_flutter/dargon2_flutter/lib/src/initializer_mobile.dart
@@ -3,7 +3,12 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import 'dart:io';
+
 import 'package:dargon2_flutter_mobile/dargon2_flutter_mobile.dart';
+import 'package:dargon2_flutter_desktop/dargon2_flutter_desktop.dart';
 import 'package:dargon2_flutter_platform_interface/dargon2_flutter_platform.dart';
 
-void initialize() => DArgon2Platform.instance = DArgon2Mobile();
+void initialize() => Platform.isAndroid || Platform.isIOS
+    ? DArgon2Platform.instance = DArgon2Mobile()
+    : DArgon2Platform.instance = DArgon2Desktop();
